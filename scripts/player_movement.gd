590bfe58e881a1b44b277d48fb3b9520576b5737
ima-cow <ethanthequag@gmail.com>
2025-09-14 16:16:52 -0400

    tried making it so that you die if you leave the area but did not work

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -5,6 +5,9 @@ const JUMP_VELOCITY = -800.0
 @export var health = 100.0
 var can_move = true
 
+func _ready() -> void:
+	$"../Play Area".body_shape_exited.connect(_on_play_area_body_shaped_exited)
+
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -22,12 +25,13 @@ func _physics_process(delta: float) -> void:
 		get_child(2).do_move(global_position, 0)
 	else:
 		velocity.x = 0
-
+	
 	move_and_slide()
 
 func _process(delta: float) -> void:
 	if health <= 0:
+		print("YOU DIED!")
 		queue_free()
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
+func _on_play_area_body_shaped_exited():
 	health = 0
