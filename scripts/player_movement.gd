77da03f645efe6baa19a3d2e2eb2950276e00db3
ima-cow <ethanthequag@gmail.com>
2025-09-14 15:37:07 -0400

    made it so you cannot move while attacking

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -3,22 +3,27 @@ extends CharacterBody2D
 const JUMP_VELOCITY = -800.0
 @export var speed = 400.0
 @export var health = 100.0
+var can_move = true
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	
-	var direction := Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * speed
+	if can_move:
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		
+		var direction := Input.get_axis("move_left", "move_right")
+		if direction:
+			velocity.x = direction * speed
+		else:
+			velocity.x = move_toward(velocity.x, 0, speed)
+		
+		get_child(2).do_move(global_position, 0)
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-	
+		velocity.x = 0
+
 	move_and_slide()
-	get_child(2).do_move(global_position, 0)
 
 func _process(delta: float) -> void:
 	if health <= 0:
