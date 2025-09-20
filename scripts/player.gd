08ffed3dbf9b3b45f76ebe563d9609c3e6813305
ima-cow <ethanthequag@gmail.com>
2025-09-20 15:31:42 -0400

    i forgr

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,13 @@
 extends CharacterBody2D
 
-const JUMP_VELOCITY := -500.0
-const JUMP_MULTIPLIER := 3
+const JUMP_VELOCITY := -550.0
+const JUMP_MULTIPLIER := 2
 @export var speed := 400.0
 @export var health := 100.0
 var can_move := true
 
-func _ready() -> void:
-	$"../PlayArea".body_shape_exited.connect(_on_play_area_body_shaped_exited)
+#func _ready() -> void:
+	#$"../PlayArea".body_shape_exited.connect(_on_play_area_body_shaped_exited)
 
 var jump_holding_amount := 1.0
 func _physics_process(delta: float) -> void:
@@ -18,8 +18,7 @@ func _physics_process(delta: float) -> void:
 		if Input.is_action_pressed("jump"):
 			if jump_holding_amount < 2:
 				jump_holding_amount += JUMP_MULTIPLIER * delta
-		
-		if Input.is_action_just_released("jump") and is_on_floor():
+		elif Input.is_action_just_released("jump") and is_on_floor():
 			velocity.y = JUMP_VELOCITY * jump_holding_amount
 			jump_holding_amount = 1
 		
