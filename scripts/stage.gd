eb6db60d2178ca02982dcb96af4c833d07c978ee
ima-cow <ethanthequag@gmail.com>
2025-09-22 12:35:39 -0400

    finished fixing these spites

--- scripts/stage.gd
+++ scripts/stage.gd
@@ -5,9 +5,9 @@ func _on_ninja_button_button_up() -> void:
 	$CharacterSelector.queue_free()
 
 func _on_cowboy_button_button_up() -> void:
-	add_child(preload("res://scenes/characters/ninja.tscn").instantiate())
+	add_child(preload("res://scenes/characters/cowboy.tscn").instantiate())
 	$CharacterSelector.queue_free()
 
 func _on_big_hands_button_button_up() -> void:
-	add_child(preload("res://scenes/characters/ninja.tscn").instantiate())
+	add_child(preload("res://scenes/characters/big_hands.tscn").instantiate())
 	$CharacterSelector.queue_free()
