10b4392bea08eac5bbae26f1c5caa476122e8b02
ima-cow <ethanthequag@gmail.com>
2025-09-24 13:56:17 -0400

    broke jumping

--- scripts/player.gd
+++ scripts/player.gd
@@ -14,7 +14,6 @@ var can_move := true
 #func _ready() -> void:
 	#$"../PlayArea".body_shape_exited.connect(_on_play_area_body_shaped_exited)
 
-var jump_holding_amount := 1.0
 func _physics_process(delta: float) -> void:
 	if multiplayer.is_server():
 		apply_movement_from_input(delta)
@@ -24,12 +23,8 @@ func apply_movement_from_input(delta: float):
 		velocity += get_gravity() * delta
 	
 	if can_move:
-		if Input.is_action_pressed("jump"):
-			if jump_holding_amount < 2:
-				jump_holding_amount += JUMP_MULTIPLIER * delta
-		elif Input.is_action_just_released("jump") and is_on_floor():
-			velocity.y = JUMP_VELOCITY * jump_holding_amount
-			jump_holding_amount = 1
+		if $InputSynchronizer.jump_released:
+			velocity.y = JUMP_VELOCITY * $InputSynchronizer.jump_holding_amount
 		
 		var direction = $InputSynchronizer.input_direction
 		if direction:
